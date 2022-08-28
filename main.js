4736f66f2b4ae0bcaaf9fa2d00e9a69fd427e89c
mwendamwenda <benmwenda83@gmail.com>
2022-08-28 12:51:07 +0300

    completed assignment

--- main.js
+++ main.js
@@ -3,6 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorSection = document.getElementById('modal')
+const heartBtn = document.getElementsByClassName('like-glyph');
+
+errorSection.className = ('hidden')
+
+for(const btn of heartBtn){
+  btn.addEventListener("click", ()=>{
+    mimicServerCall()
+
+    .then( ()=>{
+      if(btn.className === "activated-heart"){
+        btn.textContent = EMPTY_HEART;
+        btn.classList.remove("activated-heart")
+      } else{
+        btn.textContent = FULL_HEART;
+        btn.className = "activated-heart"
+      }
+    })
+    .catch((err) => {
+      errorSection.removeAttribute('class')
+      document.getElementById('modal-message').textContent = err
+      setTimeout(() => { error.setAttribute('class', 'hidden') }, 3000)
+    })
+  }
+  )
+}
 
 
 
